ea764a02b01d94d9339dc33b89c215d4e9b4f5dc
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2024-03-13 18:21:55 +0300

    Git add Aggregation and empty NewOpportunities.

---
+++ LessonsLinq/Aggregation/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//Count
+int size = numbers.Count(); 
+
+//Sum
+int sum = numbers.Sum();  
+
+//Average
+int min = numbers.Min();  
+
+//Average
+int max = numbers.Max(); 
+
+;
