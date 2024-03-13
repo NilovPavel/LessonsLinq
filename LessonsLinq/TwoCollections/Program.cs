d9a3cd9626bb761f781afdc5146287cd4b32c8fd
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2024-03-13 14:02:55 +0300

    Add methods.

--- LessonsLinq/TwoCollections/Program.cs
+++ LessonsLinq/TwoCollections/Program.cs
@@ -1,5 +1,4 @@
-﻿//Join
-using System;
+﻿using System;
 
 Person[] people =
 {
@@ -13,11 +12,37 @@ Company[] companies =
     new Company("Google", "Go"),
     new Company("Oracle", "Java")
 };
-var employees = from p in people
+
+
+//Join
+//Var1
+var var1 = from p in people
                 join c in companies on p.Company equals c.Title
                 select new { Name = p.Name, Company = c.Title, Language = c.Language };
+//Var2
+var var2 = people.Join(
+    companies, 
+    person=> person.Company, 
+    comp => comp.Title, 
+    (person, comp) => new { Name = person.Name, Lang = comp.Language } 
+    );
 
-var t = people.Join(companies, person=> person.Company, comp => comp.Title, (person, comp) => new { Name = person.Name, Lang = comp.Language } );
+
+string[] soft = { "Microsoft", "Google", "Apple" };
+string[] hard = { "Apple", "IBM", "Samsung" };
+
+//Union
+IEnumerable<string> unionCompamies = soft.Union(hard);
+
+//Concat
+IEnumerable<string> concatCompanies = soft.Concat(hard);
+
+//Except 
+IEnumerable<string> exceptCompaines = soft.Except(hard);
+
+//InterSect
+IEnumerable<string> intersectCompaines = soft.Intersect(hard);
+
 ;
 record class Person(string Name, string Company);
 record class Company(string Title, string Language);
