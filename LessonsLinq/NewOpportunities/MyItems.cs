230086539ca76618c1bf6bc3cc55611a364324a7
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2024-03-14 13:37:51 +0300

    Added new opportunities.

---
+++ LessonsLinq/NewOpportunities/MyItems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewOpportunities
+{
+    class MyItems<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- LessonsLinq/NewOpportunities/Program.cs
+++ LessonsLinq/NewOpportunities/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Chunk
+using NewOpportunities;
+
+List<Movie> movies = new List<Movie>
+{
+    new Movie("Titanic", 1998, 4.5f),
+    new Movie("The Fifth Element", 1997, 4.6f),
+    new Movie("Terminator 2", 1991, 4.7f),
+    new Movie("Avatar", 2009, 5),
+    new Movie("Platoon", 1986, 4),
+    new Movie("My Neighbor Totoro", 1988, 5)
+};
+
+IEnumerable<Movie[]> chunksOf3 = movies.Chunk(3);
+
+
+
+// TryGetNotEnumeratedCount
+int sizeOfMovies = 0;
+movies.TryGetNonEnumeratedCount(out sizeOfMovies);
+
+IEnumerable<int> arrayOne = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+bool canGetCountDirectly = arrayOne.TryGetNonEnumeratedCount(out int theCount);
+Console.WriteLine($"Array One count can be returned directly = {canGetCountDirectly}");
+Console.WriteLine($"The count for Array One = {theCount}");
+
+List<int> listOne = new List<int> { 1, 2, 3, 4, 5 };
+canGetCountDirectly = listOne.TryGetNonEnumeratedCount(out int theCountList);
+Console.WriteLine($"List One count can be returned directly = {canGetCountDirectly}");
+Console.WriteLine($"The count for List One = {theCountList}");
+
+MyItems<int> customOne = new MyItems<int>();
+canGetCountDirectly = customOne.TryGetNonEnumeratedCount(out int theCountCustom);
+Console.WriteLine($"Custom One count can be returned directly = {canGetCountDirectly}");
+Console.WriteLine($"The count for Custom One = {theCountCustom}");
+
+
+//Three way zipping
+
+
+
+//MaxBy
+Movie maxYearMovie = movies.MaxBy(item => item.ReleaseYear);
+//MinBy
+Movie minRatioMovie = movies.MinBy(item => item.Rating);
+
+
+//ElementAt
+Movie movie = movies.ElementAt(new Index (1, false));
+movie = movie.ElementAt(new Index(0, true));
+
+//Показать эксепшен
+//movie = movies.ElementAt (new Index (0, true));
+
+;
+record Movie(string Name, int ReleaseYear, float Rating);
